fb7153c13e846faba03850dd0474b4dca52a13aa
WenjieLuo2333 <43015006+WenjieLuo2333@users.noreply.github.com>
2019-02-14 15:38:02 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='modularity',
+    version='0.0.1',
+    packages=['numpy'],
+    url='',
+    license='',
+    author='Wenjie Luo',
+    author_email='wjluo@bu.edu',
+    description=''
+)
